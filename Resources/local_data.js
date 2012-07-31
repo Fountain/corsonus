0f243d2a5ba580d6daaa25c8004eb44ca88c3e6e
Aidan Feldman <aidan.feldman@gmail.com>
2012-07-31 00:05:47 -0400

    fetch remote JSON

--- Resources/local_data.js
+++ Resources/local_data.js
@@ -4,6 +4,39 @@
 var tab2;
 
 (function(){
+	var DataStore = {
+		lastUpdated: undefined, // milliseconds since epoch
+		data: undefined, // the JSON returned from the server
+		
+		fetchLatest: function(callback) {
+			var self = this;
+			var client = Ti.Network.createHTTPClient({
+				// function called when the response data is available
+				onload : function(e) {
+					Ti.API.info("Received text: " + this.responseText);
+					alert('success');
+					
+					var json = JSON.parse(this.responseText);
+					self.lastUpdated = Date.now();
+					self.data = json;
+					if (callback) callback(json);
+				},
+				// function called when an error occurs, including a timeout
+				onerror : function(e) {
+					Ti.API.debug(e.error);
+					alert('error');
+				},
+				timeout : 5000 // in milliseconds
+			});
+			// Prepare the connection.
+			client.open('GET', 'http://corsonus.com/programs/corsonus-1.json');
+			// Send the request.
+			client.send();
+		}
+	};
+	
+	DataStore.fetchLatest();
+	
 	var file = Titanium.Filesystem.getFile('events/default.json');
 	var event1 = JSON.parse(file.read().text);
 	
