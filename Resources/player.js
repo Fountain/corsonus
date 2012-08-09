5bef33d29bf2c9bc1068ca567bb881b91fdadf3e
Aidan Feldman <aidan.feldman@gmail.com>
2012-08-08 23:23:50 -0400

    change background audio configuration for iOS

--- Resources/player.js
+++ Resources/player.js
@@ -1,6 +1,9 @@
+// for iOS - note that the simulator is not capable of multitasking,
+// so won't play sounds in the background
+Ti.Media.setAudioSessionMode(Ti.Media.AUDIO_SESSION_MODE_PLAYBACK);
+
 var Player = Ti.Media.createSound({
-	allowBackground: true, // for Android
-	audioSessionMode: Ti.Media.AUDIO_SESSION_MODE_PLAYBACK // for iOS
+	allowBackground: true // for Android
 });
 
 
