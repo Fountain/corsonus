92a48c29a00a88ae478ca6e47d02be607949e763
Aidan Feldman <aidan.feldman@gmail.com>
2012-08-10 02:50:20 -0400

    change API timestamp to be milliseconds

--- api/server.rb
+++ api/server.rb
@@ -4,5 +4,6 @@ require 'json'
 get '/start.json' do
   content_type :json
   time = Time.now + 10
-  { start_time: time.to_s }.to_json
+  # in milliseconds
+  { start_time: (time.to_f * 1000).to_i }.to_json
 end
