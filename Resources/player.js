9ce200d664e462b5316cbccc257672bd2938d5a7
Aidan Feldman <aidan.feldman@gmail.com>
2012-08-14 21:41:22 -0400

    only call Ti.Media.setAudioSessionMode() on ios

--- Resources/player.js
+++ Resources/player.js
@@ -1,6 +1,9 @@
 // for iOS - note that the simulator is not capable of multitasking,
 // so won't play sounds in the background
-Ti.Media.setAudioSessionMode(Ti.Media.AUDIO_SESSION_MODE_PLAYBACK);
+var platform = Ti.Platform.getOsname();
+if (platform === 'iphone' || platform === 'ipad'){
+	Ti.Media.setAudioSessionMode(Ti.Media.AUDIO_SESSION_MODE_PLAYBACK);
+}
 
 var sound = Ti.Media.createSound({
 	allowBackground: true // for Android
