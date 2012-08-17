1a8859c060d7031344d2514984f671449c451193
Aidan Feldman <aidan.feldman@gmail.com>
2012-08-17 03:50:54 -0400

    twilio: allow user to choose song

--- api/server.rb
+++ api/server.rb
@@ -1,19 +1,32 @@
 require 'sinatra'
 require 'json'
 
-@users_by_sid = {}
-@start_time = nil
+@tracks_by_sid = {}
 
 @twilio = Twilio::REST::Client.new(
   TWILIO_SID = ENV['CORSONUS_TWILIO_SID'] || raise("please set CORSONUS_TWILIO_SID"),
   TWILIO_TOKEN = ENV['CORSONUS_TWILIO_TOKEN'] || raise("please set CORSONUS_TWILIO_TOKEN")
 )
 
-get '/call_start' do
+TRACKS = JSON.parse File.read(File.join(File.dirname(__FILE__), 'tracks.json'))
+
+get '/twilio/call' do
   response = Twilio::TwiML::Response.new do |r|
-    r.Say "Hello, and welcome to Corsonus."
+    r.Say "Welcome to Corsonus. This application has been designed to accompany a live theatrical performance.  You can always visit corsonus.com for more details.  Please choose a song."
+    
+    r.Gather action: '/twilio/choose_song', numDigits: 1 do |g|
+      g.Say "Please select an audio performance."
+      
+      TRACKS.each_with_index do |track, i|
+        g.Say "Press #{i + 1} for #{track['name']}."
+      end
+    end
   end
   response.text
+end
+
+post '/twilio/choose_song' do
+  puts params.inspect
 end
 
 post '/trigger' do
