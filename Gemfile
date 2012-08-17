d121566f0cd11b74b6e0a7d57929485227258192
Aidan Feldman <aidan.feldman@gmail.com>
2012-08-17 03:23:56 -0400

    spit out simple twilio hello world

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'http://rubygems.org'
 
-gem 'sinatra'
+gem 'sinatra', '~> 1.3'
+gem 'typhoeus', '~> 0.4'
+gem 'twilio-ruby', '~> 3.8'
--- api/server.rb
+++ api/server.rb
@@ -1,6 +1,27 @@
 require 'sinatra'
 require 'json'
 
+@users_by_sid = {}
+@start_time = nil
+
+@twilio = Twilio::REST::Client.new(
+  TWILIO_SID = ENV['CORSONUS_TWILIO_SID'] || raise("please set CORSONUS_TWILIO_SID"),
+  TWILIO_TOKEN = ENV['CORSONUS_TWILIO_TOKEN'] || raise("please set CORSONUS_TWILIO_TOKEN")
+)
+
+get '/call_start' do
+  response = Twilio::TwiML::Response.new do |r|
+    r.Say "Hello, and welcome to Corsonus."
+  end
+  response.text
+end
+
+post '/trigger' do
+  # interrupt all calls
+end
+
+
+# the iOS api
 get '/start.json' do
   content_type :json
   time = Time.now.utc
